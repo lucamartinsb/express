44e8cac2881964155a2d3411a5d80923c592749b
lucamartinsb <lucabindermartins@gmail.com>
2023-01-09 00:21:21 -0300

    Adicionado a primeira view formulario

--- index.js
+++ index.js
@@ -5,18 +5,10 @@ const handlebars = require('express-handlebars') // importando o handlebars para
 app.engine('handlebars', handlebars.engine({defaultLayout: 'main'})) // configurando o arquivo principal.
 app.set('view engine', 'handlebars') // configurando qual será o template engine.
 
-
-app.get('/', (request, response) => {
-    response.send("Este foi o ínicio de tudo!")
+app.get('/cadastrar', (req, res) => {
+    res.render('formulario')
 })
-// aqui foi definido a rota incial ou raiz e retorna uma mensagem na página.
-
-app.get('/sobre', (request, response) => {
-    response.send("Este é um projeto criado por Luca Martins e ele é feito com Express, que é um framework back-end do Node.js.")
-})
-// esta é uma rota sobre que traz um resumo do projeto como mensagem na página.
 
 app.listen(3000, () => {
     console.log('Servidor rodando na url http://localhost:3000')
-})
-// função que escuta na porta passada como parâmetro (ultima linha do código).
+}) // função que escuta na porta passada como parâmetro (ultima linha do código).
